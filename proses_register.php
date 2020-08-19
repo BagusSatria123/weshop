85199bc7a78e30ff40c6f3d164fcbb77a35f2e5d
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-19 09:29:30 +0700

    pengecekan field

--- proses_register.php
+++ proses_register.php
@@ -8,7 +8,18 @@ $nama_lengkap=$_POST['nama_lengkap'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
 $alamat=$_POST['alamat'];
-$password=md5($_POST['password']);
+$password=$_POST['password'];
 $re_password=$_POST['re-password'];
 
+unset($_POST['password']);
+unset($_POST['re-password']);
+
+$dataForm = http_build_query($_POST);
+
+
+if(empty($nama_lengkap) || empty($email) || empty($phone) || empty($alamat) || empty($password)){
+    header("location: ".BASE_URL."index.php?page=register&notif=require&$dataForm");
+}else{
+
 mysqli_query ($koneksi,"INSERT INTO user (level,nama,email,alamat,phone,password,status) VALUES('$level','$nama_lengkap','$email','$alamat','$phone','$password','$status')");
+}
