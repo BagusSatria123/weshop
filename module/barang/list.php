fd83685ed3900845ade5d4773ffc11d4d254f2c6
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-27 09:03:37 +0700

    Action Inserta data barang

--- module/barang/list.php
+++ module/barang/list.php
@@ -25,11 +25,11 @@ if(mysqli_num_rows($query) == 0){
     while($row=mysqli_fetch_assoc($query)){
         echo "<tr>
         <td class='kolom-nomor'>$no</td>
-        <td class='kiri'>$row[barang]</td>
+        <td class='kiri'>$row[nama_barang]</td>
         <td class='kiri'>$row[harga]</td>
         <td class='tengah'>$row[status]</td>
         <td class='kanan'>
-        <a class='tombol-action' href='".BASE_URL."index.php?page=my_profile&module=barang&action=form&barang_id=$row[barang_id]'>Edit</a>
+        <a class='tombol-action' href='".BASE_URL."index.php?page=my_profile&module=kategori&action=form&barang_id=$row[barang_id]'>Edit</a>
         </td>
         </tr>";
 
