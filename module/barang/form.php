18775fadda875b81ecc7a0d9aa189cffbc6955aa
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-27 22:15:12 +0700

    membuat module kota banner user

--- module/barang/form.php
+++ module/barang/form.php
@@ -28,6 +28,9 @@ if($barang_id){
     $gambar = "<img src='".BASE_URL."images/barang/$gambar' style='width: 200px; vertical-align: middle;'/>";
 }
 ?>
+
+<script src="<?php echo BASE_URL."js/ckeditor/ckeditor.js";?>"></script>
+
 <form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id";?>" method="POST" enctype="multipart/form-data">
 
     <div class="element-form">
@@ -55,9 +58,9 @@ if($barang_id){
         <span><input type="text" name="nama_barang" value="<?php echo $nama_barang;?>"/></span>
     </div>
 
-    <div class="element-form">
-        <label>Spesifikasi</label>
-        <span><textarea name="spesifikasi"><?php echo $spesifikasi;?></textarea></span>
+    <div style="margin-bottom:10px">
+        <label style="font-weight:bold">Spesifikasi</label>
+        <span><textarea name="spesifikasi" id="editor"><?php echo $spesifikasi;?></textarea></span>
     </div>
 
     <div class="element-form">
@@ -89,3 +92,7 @@ if($barang_id){
         <span><input type="submit" name="button" value="<?php echo $button;?>"/></span>
     </div>
 </form>
+
+<script>
+CKEDITOR.replace("editor");
+</script>
