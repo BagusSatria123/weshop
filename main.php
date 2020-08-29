86e6bd39009134d1f3b16679dc69018b49a73abd
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-29 20:55:27 +0700

    membuat CSS main.php

--- main.php
+++ main.php
@@ -18,10 +18,17 @@
     <div id="frame-barang">
         <ul>
             <?php
-                $query = mysqli_query($koneksi,"SELECT * FROM barang WHERE status='on'");
+                $query = mysqli_query($koneksi,"SELECT * FROM barang WHERE status='on' ORDER BY barang_id DESC LIMIT 9");
+                $no=1;
                 while($row=mysqli_fetch_assoc($query)){
+
+                    $style=false;
+                    if($no ==3){
+                        $style="style='margin-right:0px'";
+                        $no = 0;
+                    }
                     
-                    echo "<li>
+                    echo "<li $style>
                         <p class='price'>".rupiah($row['harga'])."</p>
                         <a href='".BASE_URL."index.php?page=detail&barang_id=$row[barang_id]'>
                             <img src='".BASE_URL."images/barang/$row[gambar]'/>
@@ -35,6 +42,8 @@
                         <div class='button-add-cart'>
                             <a href='".BASE_URL."tambah_keranjang.php?barang_id=$row[barang_id]'>+add to cart</a>
                         </div>";
+
+                        $no++;
                 }
                 ?>
         </ul>
