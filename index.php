100fdf0f7750584896f046e583ddf825ee7e0ac8
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-30 12:14:55 +0700

    banner setup

--- index.php
+++ index.php
@@ -21,6 +21,9 @@ $level = isset($_SESSION['level']) ? $_SESSION['level'] : false;
     <head>
         <title>weshop | Barang - barang elektronik</title>
         <link href="<?php echo BASE_URL."css/style.css";?>" type="text/css" rel="stylesheet" />
+        <script src="<?php echo BASE_URL."js/jquery-3.5.1.min.js";?>"></script>
+        <script src="<?php echo BASE_URL."js/Slides-SlidesJS-3/source/jquery.slides.min.js";?>"></script>
+
     </head>
 
     <body>
