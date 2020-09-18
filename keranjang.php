871caa3ef46f54e22ddad933528c4fbf4192c5f2
BagusSatria123 <satriabagusputra123@gmail.com>
2020-09-18 22:25:42 +0700

    update keranjang dan hapus item keranjang

--- keranjang.php
+++ keranjang.php
@@ -31,7 +31,7 @@ if($totalBarang  == 0){
                     <td class='kiri'>$nama_barang</td>
                     <td class='tengah'><input type='text' name='$barang_id' value='$quantity' class='update-quantity'/></td>
                     <td class='kanan'>".rupiah($harga)."</td>
-                    <td class='kanan'>".rupiah($total)."</td>
+                    <td class='kanan hapus_item'>".rupiah($total)."<a href='".BASE_URL."hapus_item.php?barang_id=$barang_id'>X</a></td>
                     </tr>";
 
                     $no ++;
@@ -41,3 +41,25 @@ if($totalBarang  == 0){
         
 }
 ?>
+
+<script>
+$(".update-quantity").on("input",function(e) {
+        var barang_id = $(this).attr("name");
+        var value = $(this).val();
+
+        $.ajax({
+                method: "POST",
+                url: "update_keranjang.php",
+                data: "barang_id="+barang_id+"$value="+value
+        })
+        .done(function(data)){
+                var json = $.parseJSON(data);
+                        if(json.status == true){
+                         location.reload();
+                }else{
+                         alert(json.pesan);
+                        location.reload();
+                }
+        });
+});
+</script>
